61eb4a48f509e94a44fbb23d718d4b96d3b31262
swapnali12deokar <swapnalideokar0307@gmail.com>
2019-12-14 15:17:14 +0530

    Initial commit from Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>ROBO FRIENDS</h1>
+      <input type="text" palceholder="search robots"/>
+    </div>
+  );
+}
+
+export default App;
