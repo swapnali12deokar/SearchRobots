7e7e9b34ba808d0b052b774e89516e10164b43ec
swapnali12deokar <swapnalideokar0307@gmail.com>
2019-12-21 18:25:16 +0530

    Updates

---
+++ precache-manifest.58319ae9e62fade9282ee86719c3bd11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7656c14384ef5001851ea116d8c7ae7",
+    "url": "/SearchRobots/index.html"
+  },
+  {
+    "revision": "6b75c7ed110c2128ba99",
+    "url": "/SearchRobots/static/css/main.6a6b0ffb.chunk.css"
+  },
+  {
+    "revision": "76e85e66ec30cd508745",
+    "url": "/SearchRobots/static/js/2.773078cc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/SearchRobots/static/js/2.773078cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b75c7ed110c2128ba99",
+    "url": "/SearchRobots/static/js/main.fc479274.chunk.js"
+  },
+  {
+    "revision": "49106824a9fa9160850a",
+    "url": "/SearchRobots/static/js/runtime-main.743f0825.js"
+  }
+]);
